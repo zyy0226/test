ae94e7702325b8f1671c6660a73206d9e02f7c57
yy <334525855@qq.com>
2018-02-24 10:42:34 +0800

    Initial Commit

---
+++ xdasadasdsa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xdasadasdsa
+//
+//  Created by 陶亚利 on 2018/2/24.
+//  Copyright © 2018年 陶亚利. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xdasadasdsa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xdasadasdsa
+//
+//  Created by 陶亚利 on 2018/2/24.
+//  Copyright © 2018年 陶亚利. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
