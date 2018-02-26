a37b364568dca61124a5e85d5cc999671f7194f9
yy <334525855@qq.com>
2018-02-26 17:21:51 +0800

    test

--- xdasadasdsa/ViewController.m
+++ xdasadasdsa/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#import <pthread.h>
 
 @interface ViewController ()
+
+@property (nonatomic,strong) NSThread *threadA;
+
+@property (nonatomic,strong) NSThread *threadB;
+
+@property (nonatomic,strong) NSThread *threadC;
 
 @end
 
@@ -16,9 +23,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _threadA = [[NSThread alloc] initWithTarget:self selector:@selector(saleTicket) object:nil];
+    _threadB = [[NSThread alloc] initWithTarget:self selector:@selector(saleTicket) object:nil];
+    _threadC = [[NSThread alloc] initWithTarget:self selector:@selector(saleTicket) object:nil];
+//    pthread_t thread;
+//
+//    pthread_create(&thread, NULL, task, NULL);
+//
+//    pthread_t thread1;
+//
+//    pthread_create(&thread1, NULL, task, NULL);
+//
+//    NSThread *thread2 = [[NSThread alloc] initWithTarget:self selector:@selector(haha:) object:@"123"];
+//
+//    [thread2 start];
+//
+//    [NSThread detachNewThreadSelector:@selector(haha:)  toTarget:self withObject:@"xixi"];
+    
 }
 
+- (void)saleTicket {
+    
+    
+}
+
+- (void)haha:(NSString *)str {
+    
+    NSLog(@"%@-------",[NSThread currentThread]);
+}
+
+void *task (void * para) {
+    
+    NSLog(@"%@--------",[NSThread currentThread]);
+    return NULL;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
